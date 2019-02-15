41858c4ffd5842c6a57718f2e664c5524aa99429
Guest User <Guest@epicodus-5.local>
2019-02-14 16:00:17 -0800

    it will practice importing img

--- img-webpack/src/components/Header.jsx
+++ img-webpack/src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-
+import dog from '../../assests/img/dog.jpg';
 function Header(){
   return (
     <div>
       <h1>Help Queue</h1>
+      <img src={dog} alt=""/>
       <Link to="/">Home</Link> | <Link to="/newticket">Create Ticket</Link>
     </div>
   );
